b3b54fe63b251af6e7a72a2be48dd1f39156c24c
Emilia234 <117676498+Emilia234@users.noreply.github.com>
2024-10-30 13:28:22 +0100

    Update grafika.cpp

--- grafika.cpp
+++ grafika.cpp
@@ -2,10 +2,6 @@
 #include <SFML/Window.hpp>
 #include <iostream>
 #include <cmath>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
